82e9f95006108e61a407debd938b8662cf6d1173
Harshal Kumbhalkar <thejusthrk@gmail.com>
2023-08-26 12:51:37 +0530

    Add files via upload

---
+++ p1.py
@@ -0,0 +1,5 @@
+n=int(input("Enter a Number:"))
+i=1
+while(i<=10):
+    print("%d x %d = %d" %(n,i,n*i))
+    i+=1
---
+++ string.py
@@ -0,0 +1,7 @@
+str="This is a sample string to search for specific character."
+x=input("Enter characters:")
+y=str.find(x)
+if y==-1:
+    print(0)
+else:
+    print(1)
